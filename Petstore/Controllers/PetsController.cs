ab763ef288acaa666e94b8a29dcb2697843f427d
simonmungi <simonmungi@gmail.com>
2020-04-14 19:04:21 -0300

    Commit Inicial

---
+++ Petstore/Controllers/PetsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Petstore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetsController : ControllerBase
+    {
+        private static List<Pets> listPets;
+
+        private readonly ILogger<PetsController> _logger;
+
+        public PetsController(ILogger<PetsController> logger)
+        {
+            _logger = logger;
+
+            if(listPets== null){
+            
+                listPets = new List<Pets>();
+
+                listPets.Add(new Pets(){
+                    id="233",
+                    name="collar perro",
+                    tag="perro"
+                });
+            }
+
+
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Pets> Get()
+        {
+            return listPets;
+        }
+
+        [HttpPost]
+        public Pets Post(Pets bodyParam){
+            listPets.Add(bodyParam);
+            return bodyParam;
+
+        }
+    }
+}
--- Petstore/Pets.cs
+++ Petstore/Pets.cs
@@ -2,14 +2,18 @@ using System;
 
 namespace Petstore
 {
-    public class WeatherForecast
+    public class Pets
     {
+        /*
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        */
 
-        public string Summary { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string tag { get; set; }
     }
 }
