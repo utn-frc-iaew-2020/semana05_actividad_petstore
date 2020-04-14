e9ecdba860f518a4c769c2ee99f61e734bad76cb
uriel nicolas risso <uriel_nr_18_6@hotmail.com>
2020-04-14 19:05:24 -0300

    get de los pets

---
+++ Petstore/Controllers/PetsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Petstore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetsController : ControllerBase
+    {
+        private List<Pets> listPets;
+        private readonly ILogger<PetsController> _logger;
+
+        public PetsController(ILogger<PetsController> logger)
+        {
+            _logger = logger;
+            listPets = new List<Pets>();
+            listPets.Add(new Pets(){
+                id = "123",
+                name = "collar perro",
+                tag = "perro",
+            });
+        }
+
+        [HttpGet]
+        public IEnumerable<Pets> Get()
+        {
+            return listPets;
+        }
+    }
+}
---
+++ Petstore/Pets.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Petstore
+{
+    public class Pets
+    {
+        public string id {get; set;}
+        public string name {get; set;}
+        public string tag {get; set;}
+
+    }
+}
